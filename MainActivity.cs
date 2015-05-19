25af341d6dae1dbd50c9652404a6a696473e0b9b
parmendrak <parmendrak@360logica.com>
2015-05-20 00:02:13 +0530

    new

--- MainActivity.cs
+++ MainActivity.cs
@@ -24,7 +24,7 @@ namespace Androidk
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
+			Console.WriteLine ("cxfds");
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
