a3f7e9b397d5dad3d1a4484df56f97c25ebc799e
parmendrak <parmendrak@360logica.com>
2015-05-20 00:24:30 +0530

    u

--- MainActivity.cs
+++ MainActivity.cs
@@ -24,7 +24,7 @@ namespace Androidk
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button> (Resource.Id.myButton);
-		
+			Console.WriteLine ("parm");
 			Console.WriteLine ("parm");
 			Console.WriteLine ("parm");
 
